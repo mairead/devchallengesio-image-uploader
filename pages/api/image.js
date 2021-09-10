a5d21cd60d54cacba7e49e8782189d0d4df7d1b0
Mairead Preece <mairead.preece@epimorphics.com>
2021-09-10 21:37:38 +0100

    Adding backend implementation and TODO notes for stuff to research later

--- pages/api/image.js
+++ pages/api/image.js
@@ -1,3 +1,60 @@
-export default function handler(req, res) {
-    res.status(200).json({filename: req.body.name});
+import Formidable from "Formidable";
+const fs = require("fs");
+
+// TODO learn what is this config stuff?
+export const config = {
+  api: {
+    bodyParser: false
+  }
+};
+
+// what is this double chained syntax? curried?
+const uploadImage = next => (req, res) => {
+  // TODO is this promise syntax ok?
+  // promise stuff seems like voodoo
+  return new Promise(async (resolve, reject) => {
+    try {
+      const form = new Formidable.IncomingForm({
+        multiples: true,
+        keepExtensions: true
+      });
+      form.once("error", console.error);
+      await form.parse(req, async (err, fields, files) => {
+        if (err) {
+          throw String(JSON.stringify(err, null, 2));
+        }
+        console.log(
+          "moving file: ",
+          files.file.path,
+          " to ",
+          `public/upload/${files.file.name}`
+        );
+        fs.renameSync(files.file.path, `public/upload/${files.file.name}`);
+        req.form = { fields, files };
+        return resolve(next(req, res));
+      });
+    } catch (error) {
+      return resolve(res.status(403).send(error));
+    }
+  });
+}  
+
+const handler = (req, res) => {
+  try {
+    if (req.method === "POST") {
+      res.status(200).send(req.form);
+    } else {
+      throw String("Method not allowed");
+    }
+  } catch (error) {
+    res.status(400).json({ message: JSON.stringify(error, null, 2) });
+  }
 }
+
+// TODO what is stringify null 2 doing here?
+// export default function handler(req, res) {
+//     // nothing returned in response?
+//     res.status(200).json({filename: req.body.name});
+// }
+
+export default uploadImage(handler);
--- pages/index.js
+++ pages/index.js
@@ -3,49 +3,113 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+// TODO form action and submit as no-js fallback
+// TODO move separate views and logic into separate components
+// TODO Create separate module CSS  - what is .module?
+// TODO hide preview panel
+// TODO fix double file browser appearing?
+// TODO body exceeded 1MB limit
+// TODO unit tests?
+
 export default function Home() {
-  const [fileSelected, setFileSelected] = useState();
-  const [showLoadingDialog, setShowLoadingDialog] = useState();
+  // const [fileSelected, setFileSelected] = useState();
+  const [showLoadingDialog, setShowLoadingDialog] = useState(false);
+  const [showFileUpload, setShowFileUpload] = useState(true);
+  const [showPreview, setShowPreview] = useState(false);
+
+  const [dragActive, setDragActive] = useState(false);
 
   const inputFile = useRef(null);
+  const imagePreview = useRef(null);
 
-  const onOpenFileInput = () => {
+  const onOpenFileInput = (e) => {
     inputFile.current.click();
+    e.preventDefault();
   }
 
   const onFileChange = (e) => {
-    console.log(e.target.files[0]);
-    setFileSelected(e.target.files[0]);
-    setShowLoadingDialog(true);
+    const fileObj = e.target.files[0];
+    onFileSelected(fileObj);
+    // Need to empty onFileChange after so it can
+    // reselect the same file again
+    // setFileSelected(null);
 
-    const body = new FormData();
-    body.append('file', fileSelected);
-    fetch("/api/upload", {
-      method: "POST",
-      body
-    });
-
-    setFileSelected(null);
+    e.preventDefault();
   }
 
   const onDragOver = (e) => {
+    setDragActive(true);
     e.preventDefault();
     e.stopPropagation();
   }
 
   const onDragEnter = (e) => {
+    setDragActive(true);
     e.preventDefault();
     e.stopPropagation();
   }
 
   const onDragLeave = (e) => {
+    setDragActive(false);
     e.preventDefault();
     e.stopPropagation();
   }
 
   const onDrop = (e) => {
+    setDragActive(false);
+
+    const fileObj = e.dataTransfer.files[0];
+    onFileSelected(fileObj);
+
     e.preventDefault();
     e.stopPropagation();
+  }
+
+  const onUploadFile = (fileObj) => {
+    const body = new FormData();
+    body.append('file', fileObj);
+    fetch("/api/image", {
+      method: "POST",
+      body
+    })
+    .then(() => {
+      setShowLoadingDialog(false);
+      setShowPreview(true);
+      setShowFileUpload(false);
+      console.log('network returned as 200');
+      // show uploaded image
+    })
+    .catch(() => {
+      // whats best error handling pattern?
+    });
+    // is this then/catch out of date?
+    // should I be using async/await instead?
+  }
+
+  const onFileSelected = (fileObj) => {
+    setShowLoadingDialog(true);
+    onPreviewFile(fileObj);
+    onUploadFile(fileObj)
+    console.log(fileObj);
+  }
+
+  // is this going to change correctly on each render?
+  const draggableClasses = ['draggable-area'];
+  if (dragActive) {
+    draggableClasses.push('draggable-area--active');
+  }
+
+  const previewClasses = ['panel', 'preview'];
+  if (showPreview) {
+    previewClasses.push('preview--active');
+  }
+
+  const onPreviewFile = (fileObj) => {
+    let reader = new FileReader();
+    reader.readAsDataURL(fileObj);
+    reader.onloadend = () => {
+      imagePreview.current.src = reader.result;
+    }
   }
 
   return (
@@ -54,34 +118,55 @@ export default function Home() {
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <Header title="Upload your image" />
-        <p className="description">
-          Files should be Jpeg, Png...
-        </p>
-        <div className="draggable-area"
-          onDragOver={onDragOver}
-          onDragEnter={onDragEnter}
-          onDragLeave={onDragLeave}
-          onDrop={onDrop}
-        >
-          <img src="/image.svg" alt="placeholder image showing scenic view"/>
-          <p>Drag and Drop your image here</p>
-        </div>
-        <p>Or</p>
-        <input
-          type="file"
-          ref={inputFile}
-          className="file-input hidden"
-          onChange={onFileChange}
-        />
-        <button onClick={onOpenFileInput}>Choose a file</button>
+        {showFileUpload && (
+          <div className="panel">
+            <Header title="Upload your image" />
+            <p className="description">
+              Files should be Jpeg, Png...
+            </p>
+            <div className={draggableClasses.join(' ')}
+              onDragOver={onDragOver}
+              onDragEnter={onDragEnter}
+              onDragLeave={onDragLeave}
+              onDrop={onDrop}
+            >
+              <img src="/image.svg" alt="placeholder image showing scenic view" />
+              <p>Drag and Drop your image here</p>
+            </div>
+            <p>Or</p>
+            <form>
+              <label
+                htmlFor="fileInput"
+                onClick={onOpenFileInput}
+                className="button"
+              >
+                Choose a file
+              </label>
+              <input
+                id="fileInput"
+                type="file"
+                ref={inputFile}
+                className="file-input hidden"
+                onChange={onFileChange}
+              />
+            </form>
+          </div>
+        )}
         {showLoadingDialog && (
-          <div className="loading-dialog">
+          <div className="panel loading-dialog">
             <p>Uploading....</p>
           </div>
         )}
+        <div className={previewClasses.join(' ')}>
+          <p>Uploaded successfully!</p>
+          <img
+            className="preview__image"
+            src=""
+            alt="uploaded image"
+            ref={imagePreview} 
+          />
+        </div>
       </main>
       <Footer />
     </div>
